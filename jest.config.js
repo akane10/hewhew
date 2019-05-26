5de6f9c74d331b0e327911cedf34b0f085781517
yapie <targaryen.akane@gmail.com>
2019-05-26 19:44:15 +0700

    delete jest config file

--- jest.config.js
+++
@@ -1,7 +0,0 @@
-const { defaults } = require('jest-config');
-module.exports = {
-  // ...
-  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
-  // ...
-  coveragePathIgnorePatterns: ['/node_modules/', '/boilerplates/']
-};
